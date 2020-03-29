650305bb2d95076294085d7121c9b3642fbfee2e
Yuki Imai <yuki08281993@gmail.com>
2020-03-29 21:34:07 +0900

    sourceの実装の使用方法をスカイフックからファクトリに変更

--- __tests__/primitiveParsers.spec.ts
+++ __tests__/primitiveParsers.spec.ts
@@ -1,28 +1,28 @@
 import { ResultParser, ZeroParser, ItemParser } from "../src/primitiveParsers";
-import { IndexedStringSource } from "../src/indexedStringSource";
+import { NewSource } from "../src/source";
 
 describe("ResultParser", () => {
   it("should return the given value without consuming input", () => {
-    const res = ResultParser("input")(new IndexedStringSource("abcde"));
-    expect(res).toEqual([["input", new IndexedStringSource("abcde")]]);
+    const res = ResultParser("input")(NewSource("abcde"));
+    expect(res).toEqual([["input", NewSource("abcde")]]);
   });
 });
 
 describe("ZeroParser", () => {
   it("should not return any results", () => {
-    const res = ZeroParser()(new IndexedStringSource("abcde"));
+    const res = ZeroParser()(NewSource("abcde"));
     expect(res).toEqual([]);
   });
 });
 
 describe("ItemParser", () => {
   it("should return the pair of the first letter and the rest.", () => {
-    const res = ItemParser(new IndexedStringSource("abcde"));
-    expect(res).toEqual([["a", new IndexedStringSource("abcde").tail]]);
+    const res = ItemParser(NewSource("abcde"));
+    expect(res).toEqual([["a", NewSource("abcde").tail]]);
   });
 
   it("should not return any results if the source is empty.", () => {
-    const res = ItemParser(new IndexedStringSource(""));
+    const res = ItemParser(NewSource(""));
     expect(res).toEqual([]);
   });
 });
