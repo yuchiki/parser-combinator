14beb738809321f7cd13c02e38d9b2bf76ffe838
Yuki Imai <yuki08281993@gmail.com>
2020-04-05 13:45:49 +0900

    digit parser

--- src/elementaryParsers.ts
+++ src/elementaryParsers.ts
@@ -3,3 +3,7 @@ import { Sat } from "./parserCombinators";
 import { Parser } from "./parser";
 
 export const CharParser = (c: char): Parser<char> => Sat(y => c === y);
+
+export const DigitParser: Parser<char> = Sat(x =>
+  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(x),
+);
