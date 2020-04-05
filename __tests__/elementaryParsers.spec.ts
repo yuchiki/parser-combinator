be4c876500236ba1f5c7e796c3208ae440edd43a
yuchiki <yuki08281993@gmail.com>
2020-04-05 13:48:47 +0900

    digit parserを実装 (#22)

--- __tests__/elementaryParsers.spec.ts
+++ __tests__/elementaryParsers.spec.ts
@@ -1,4 +1,4 @@
-import { CharParser } from "../src/elementaryParsers";
+import { CharParser, DigitParser } from "../src/elementaryParsers";
 import { NewSource } from "../src/source";
 
 describe("CharParser", () => {
@@ -14,6 +14,25 @@ describe("CharParser", () => {
   it("should fail when the given string does not start with the given letter", () => {
     const parser = CharParser("a");
     const res = parser(NewSource("bcd"));
+    expect(res.length).toBe(0);
+  });
+});
+
+describe("DigitParser", () => {
+  it("should parse digits", () => {
+    for (let i = 0; i < 10; i++) {
+      const suffix = "abc";
+      const source = NewSource(i.toString() + suffix);
+      const res = DigitParser(source);
+      expect(res.length).toBe(1);
+      const [digit, rest] = res[0];
+      expect(digit).toBe(i.toString());
+      expect(rest.currentString).toBe(suffix);
+    }
+  });
+
+  it("should fail if the prefix of the source is not a digit", () => {
+    const res = DigitParser(NewSource("abc"));
     expect(res.length).toBe(0);
   });
 });
