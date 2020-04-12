e1ecbfd5742a44078b94308ca4a0a516be27eecf
yuchiki <yuki08281993@gmail.com>
2020-04-12 13:15:36 +0900

    lower letter parserを実装 (#23)

--- __tests__/elementaryParsers.spec.ts
+++ __tests__/elementaryParsers.spec.ts
@@ -1,4 +1,4 @@
-import { CharParser, DigitParser } from "../src/elementaryParsers";
+import { CharParser, DigitParser, LowerParser } from "../src/elementaryParsers";
 import { NewSource } from "../src/source";
 
 describe("CharParser", () => {
@@ -34,5 +34,24 @@ describe("DigitParser", () => {
   it("should fail if the prefix of the source is not a digit", () => {
     const res = DigitParser(NewSource("abc"));
     expect(res.length).toBe(0);
+  });
+});
+
+describe("LowerParser", () => {
+  it("should parse a lower letter", () => {
+    for (const c of [..."abcdefghijklmnopqrstuvwxyz"]) {
+      const res = LowerParser(NewSource(c));
+      expect(res.length).toBe(1);
+      const [cRes, rest] = res[0];
+      expect(cRes).toBe(c);
+      expect(rest.currentString).toBe("");
+    }
+  });
+
+  it("should fail otherwise", () => {
+    for (const c of [..."A1- ,.?_"]) {
+      const res = LowerParser(NewSource(c));
+      expect(res.length).toBe(0);
+    }
   });
 });
--- src/elementaryParsers.ts
+++ src/elementaryParsers.ts
@@ -7,3 +7,5 @@ export const CharParser = (c: char): Parser<char> => Sat(y => c === y);
 export const DigitParser: Parser<char> = Sat(x =>
   ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(x),
 );
+
+export const LowerParser: Parser<char> = Sat(x => /[a-z]/.test(x));
