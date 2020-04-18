4be1bd62c0b22fbdd4b89ce95a94d1483b2d18ef
Yuki Imai <yuki08281993@gmail.com>
2020-04-18 22:51:23 +0900

    plus without test

--- src/primitiveParsers.ts
+++ src/primitiveParsers.ts
@@ -9,3 +9,7 @@ export const ZeroParser = <A>(): Parser<A> => (_: Source): [A, Source][] => [];
 
 export const ItemParser = (input: Source): [char, Source][] =>
   input.isEOF ? [] : [[input.head, input.tail]];
+
+export const Plus = <A>(p1: Parser<A>, p2: Parser<A>): Parser<A> => (
+  input,
+): [A, Source][] => p1(input).concat(p2(input));
